457a60b7bb121a936de3eba5fbde008878daea5d
lahaxe <rom.lahaxe@gmail.com>
2020-03-24 11:07:34 +0100

    Ajout des premiers fichiers du modèle

---
+++ src/lib/model/core/EntityBase.h
@@ -0,0 +1,11 @@
+#ifndef _d9cb6f7b_3646_4fa3_92ec_afdc25443782
+#define _d9cb6f7b_3646_4fa3_92ec_afdc25443782
+
+class EntityBase
+{
+public:
+protected:
+private:
+};
+
+#endif // _d9cb6f7b_3646_4fa3_92ec_afdc25443782
---
+++ src/lib/model/users/User.h
@@ -0,0 +1,13 @@
+#ifndef _785f3a4a_bd4b_4116_a2f2_5bbd9c2fb9fc
+#define _785f3a4a_bd4b_4116_a2f2_5bbd9c2fb9fc
+
+#include "model/core/EntityBase.h"
+
+class User : public EntityBase
+{
+public:
+protected:
+private:
+};
+
+#endif // _785f3a4a_bd4b_4116_a2f2_5bbd9c2fb9fc
