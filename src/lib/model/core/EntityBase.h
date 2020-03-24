c5663adaaf2f01702f80dcf95ed1f6af2a889754
lahaxe <rom.lahaxe@gmail.com>
2020-03-24 11:59:02 +0100

    Implementation de la classe EntityBase

--- src/lib/model/core/EntityBase.h
+++ src/lib/model/core/EntityBase.h
@@ -1,11 +1,25 @@
 #ifndef _d9cb6f7b_3646_4fa3_92ec_afdc25443782
 #define _d9cb6f7b_3646_4fa3_92ec_afdc25443782
 
+// Include Standard files
+#include <string>
+
 class EntityBase
 {
 public:
+    EntityBase(std::string const & id = "");
+
+    virtual ~EntityBase();
+
+    std::string get_id() const;
+
+    void set_id(std::string const & id);
+
 protected:
+
 private:
+    std::string _id;
+
 };
 
 #endif // _d9cb6f7b_3646_4fa3_92ec_afdc25443782
