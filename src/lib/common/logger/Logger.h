29a8bbb11e8462868ce95d601618b5d2e4d4227c
lahaxe <rom.lahaxe@gmail.com>
2020-03-24 18:16:27 +0100

    Ajout des commentaires dans le fichier logger

--- src/lib/common/logger/Logger.h
+++ src/lib/common/logger/Logger.h
@@ -10,32 +10,74 @@ namespace libcams
 namespace common
 {
 
+/**
+ * @brief The Logger class
+ */
 class Logger
 {
 public:
+    /**
+     * @brief Create and return an unique instance of Logger
+     * @return unique instance of Logger
+     */
     static Logger& instance();
 
+    /**
+     * @brief Remove the unique instance of Logger
+     */
     static void delete_instance();
 
+    /**
+     * @brief Destructor
+     */
     virtual ~Logger();
 
+    /**
+     * @brief Write DEBUG line
+     * @param message: Text to log
+     */
     void debug(std::string const & message);
 
+    /**
+     * @brief Write INFO line
+     * @param message: Text to log
+     */
     void info(std::string const & message);
 
+    /**
+     * @brief Write WARNING line
+     * @param message: Text to log
+     */
     void warning(std::string const & message);
 
+    /**
+     * @brief Write ERROR line
+     * @param message: Text to log
+     */
     void error(std::string const & message);
 
+    /**
+     * @brief Write FATAL line
+     * @param message: Text to log
+     */
     void fatal(std::string const & message);
 
 protected:
 
 private:
+    /**
+     * @brief Create an instance of Logger
+     */
     Logger();
 
+    /// Unique instance
     static Logger* _instance;
 
+    /**
+     * @brief Write line
+     * @param message: Text to log
+     * @param level: Log level
+     */
     void write(std::string const & message, std::string const & level);
 
 };
