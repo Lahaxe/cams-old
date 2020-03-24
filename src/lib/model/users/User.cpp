d5365496d131d4c73debdc45ea897768569f481e
lahaxe <rom.lahaxe@gmail.com>
2020-03-24 14:23:08 +0100

    Implémentation de la classe User

---
+++ src/lib/model/users/User.cpp
@@ -0,0 +1,28 @@
+#include "model/users/User.h"
+
+User
+::User(std::string const & id, std::string const & name):
+    EntityBase(id), _name(name)
+{
+    //Nothing to do
+}
+
+User
+::~User()
+{
+    // Nothing to do
+}
+
+std::string
+User
+::get_name() const
+{
+    return this->_name;
+}
+
+void
+User
+::set_name(std::string const & name)
+{
+    this->_name = name;
+}
--- src/lib/model/users/User.h
+++ src/lib/model/users/User.h
@@ -6,8 +6,19 @@
 class User : public EntityBase
 {
 public:
+    User(std::string const & id = "", std::string const & name = "");
+
+    virtual ~User();
+
+    std::string get_name() const;
+
+    void set_name(std::string const & name);
+
 protected:
+
 private:
+    std::string _name;
+
 };
 
 #endif // _785f3a4a_bd4b_4116_a2f2_5bbd9c2fb9fc
