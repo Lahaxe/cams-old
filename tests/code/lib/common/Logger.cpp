40bd3d6ea82f29d3351e2e34e72336c5e4202adc
lahaxe <rom.lahaxe@gmail.com>
2020-03-25 11:42:47 +0100

    Ajout d'une fixture pour les TU de la classe Logger

--- tests/code/lib/common/Logger.cpp
+++
@@ -1,7 +0,0 @@
-#define BOOST_TEST_MODULE ModuleLogger
-
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(Constructor)
-{
-}
---
+++ tests/code/lib/common/logger/Logger.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE ModuleLogger
+
+// Include Project files
+#include "FixtureLogger.h"
+
+BOOST_FIXTURE_TEST_CASE(Constructor, FixtureLogger)
+{
+    // Nothing to do
+}
