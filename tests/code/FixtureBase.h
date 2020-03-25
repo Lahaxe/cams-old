4a43da4b110af5a663b8983efdd46e6cfc5eeae5
lahaxe <rom.lahaxe@gmail.com>
2020-03-25 11:13:03 +0100

    Ajout d'une fixture de base pour les TU

---
+++ tests/code/FixtureBase.h
@@ -0,0 +1,26 @@
+#ifndef _40b8fb44_e7ce_40c9_bb50_b435a321c48a
+#define _40b8fb44_e7ce_40c9_bb50_b435a321c48a
+
+// Include Boost library
+#include <boost/test/unit_test.hpp>
+
+class FixtureBase
+{
+public:
+    FixtureBase()
+    {
+        // Nothing to do
+    }
+
+    virtual ~FixtureBase()
+    {
+        //Nothing to do
+    }
+
+protected:
+
+private:
+
+};
+
+#endif // _40b8fb44_e7ce_40c9_bb50_b435a321c48a
