4d9038919c349092aaf451c03f476f0b02c67de4
lahaxe <rom.lahaxe@gmail.com>
2020-03-27 11:38:45 +0100

    Ajout du parsing des arguments

---
+++ src/lib/camscli/Arguments.cpp
@@ -0,0 +1,256 @@
+// Include Project files
+#include "camscli/Arguments.h"
+#include "common/exception/CamsException.h"
+
+namespace libcams
+{
+
+namespace camscli
+{
+
+Arguments
+::Arguments(int argc, char *argv[]):
+    _display_help(false), _action(""), _controller(""),
+    _user(""), _password(""), _token(""),
+    _json_file(""), _json_string(""), _ressource_path("")
+{
+    this->parse(argc, argv);
+}
+
+Arguments
+::~Arguments()
+{
+    // Nohting to do
+}
+
+bool
+Arguments
+::get_display_help() const
+{
+    return this->_display_help;
+}
+
+void
+Arguments
+::set_display_help(bool display_help)
+{
+    this->_display_help = display_help;
+}
+
+std::string
+Arguments
+::get_action() const
+{
+    return this->_action;
+}
+
+void
+Arguments
+::set_action(std::string const & action)
+{
+    this->_action = action;
+}
+
+std::string
+Arguments
+::get_controller() const
+{
+    return this->_controller;
+}
+
+void
+Arguments
+::set_controller(std::string const & controller)
+{
+    this->_controller = controller;
+}
+
+std::string
+Arguments
+::get_user() const
+{
+    return this->_user;
+}
+
+void
+Arguments
+::set_user(std::string const & user)
+{
+    this->_user = user;
+}
+
+std::string
+Arguments
+::get_password() const
+{
+    return this->_password;
+}
+
+void
+Arguments
+::set_password(std::string const & password)
+{
+    this->_password = password;
+}
+
+std::string
+Arguments
+::get_token() const
+{
+    return this->_token;
+}
+
+void
+Arguments
+::set_token(std::string const & token)
+{
+    this->_token = token;
+}
+
+std::string
+Arguments
+::get_json_file() const
+{
+    return this->_json_file;
+}
+
+void
+Arguments
+::set_json_file(std::string const & json_file)
+{
+    this->_json_file = json_file;
+}
+
+std::string
+Arguments
+::get_json_string() const
+{
+    return this->_json_string;
+}
+
+void
+Arguments
+::set_json_string(std::string const & json_string)
+{
+    this->_json_string = json_string;
+}
+
+std::string
+Arguments
+::get_ressource_path() const
+{
+    return this->_ressource_path;
+}
+
+void
+Arguments
+::set_ressource_path(std::string const & ressource_path)
+{
+    this->_ressource_path = ressource_path;
+}
+
+void Arguments::parse(int argc, char *argv[])
+{
+    for (int current_arg = 1; current_arg < argc; ++current_arg)
+    {
+        if (std::string(argv[current_arg]) == std::string("help") ||
+            std::string(argv[current_arg]) == std::string("--help") ||
+            std::string(argv[current_arg]) == std::string("-h"))
+        {
+            this->set_display_help(true);
+            break;
+        }
+        else if (current_arg == 1)
+        {
+            this->set_action(std::string(argv[current_arg]));
+        }
+        else if (current_arg == 2)
+        {
+            this->set_controller(std::string(argv[current_arg]));
+        }
+        else if (std::string(argv[current_arg]) == std::string("--user") ||
+            std::string(argv[current_arg]) == std::string("-u"))
+        {
+            if (argc < current_arg + 2)
+            {
+                throw common::CamsException("Missing some arguments");
+            }
+
+            ++current_arg;
+            this->set_user(argv[current_arg]);
+        }
+        else if (std::string(argv[current_arg]) == std::string("--password") ||
+            std::string(argv[current_arg]) == std::string("-p"))
+        {
+            if (argc < current_arg + 2)
+            {
+                throw common::CamsException("Missing some arguments");
+            }
+
+            ++current_arg;
+            this->set_password(argv[current_arg]);
+        }
+        else if (std::string(argv[current_arg]) == std::string("--token") ||
+            std::string(argv[current_arg]) == std::string("-t"))
+        {
+            if (argc < current_arg + 2)
+            {
+                throw common::CamsException("Missing some arguments");
+            }
+
+            ++current_arg;
+            this->set_token(argv[current_arg]);
+        }
+        else if (std::string(argv[current_arg]) == std::string("--jsonfile") ||
+            std::string(argv[current_arg]) == std::string("-f"))
+        {
+            if (argc < current_arg + 2)
+            {
+                throw common::CamsException("Missing some arguments");
+            }
+
+            ++current_arg;
+            this->set_json_file(argv[current_arg]);
+        }
+        else if (std::string(argv[current_arg]) == std::string("--json") ||
+            std::string(argv[current_arg]) == std::string("-j"))
+        {
+            if (argc < current_arg + 2)
+            {
+                throw common::CamsException("Missing some arguments");
+            }
+
+            ++current_arg;
+            this->set_json_string(argv[current_arg]);
+        }
+        else if (std::string(argv[current_arg]) == std::string("--ressource") ||
+            std::string(argv[current_arg]) == std::string("-r"))
+        {
+            if (argc < current_arg + 2)
+            {
+                throw common::CamsException("Missing some arguments");
+            }
+
+            ++current_arg;
+            this->set_ressource_path(argv[current_arg]);
+        }
+        else
+        {
+            throw common::CamsException("Unknwon argument");
+        }
+    }
+
+    if (this->get_display_help() == true)
+    {
+        return;
+    }
+
+    if (argc < 3)
+    {
+        throw common::CamsException("Missing some arguments");
+    }
+}
+
+} // namespace camscli
+
+} // namespace libcams
---
+++ src/lib/camscli/Arguments.h
@@ -0,0 +1,85 @@
+#ifndef _f0a9ee97_83a8_4aa1_be9d_432ad9054c1c
+#define _f0a9ee97_83a8_4aa1_be9d_432ad9054c1c
+
+// Include Standard library
+#include <string>
+
+namespace libcams
+{
+
+namespace camscli
+{
+
+class Arguments
+{
+public:
+    Arguments(int argc, char *argv[]);
+
+    virtual ~Arguments();
+
+    bool get_display_help() const;
+
+    void set_display_help(bool display_help);
+
+    std::string get_action() const;
+
+    void set_action(std::string const & action);
+
+    std::string get_controller() const;
+
+    void set_controller(std::string const & controller);
+
+    std::string get_user() const;
+
+    void set_user(std::string const & user);
+
+    std::string get_password() const;
+
+    void set_password(std::string const & password);
+
+    std::string get_token() const;
+
+    void set_token(std::string const & token);
+
+    std::string get_json_file() const;
+
+    void set_json_file(std::string const & json_file);
+
+    std::string get_json_string() const;
+
+    void set_json_string(std::string const & json_string);
+
+    std::string get_ressource_path() const;
+
+    void set_ressource_path(std::string const & ressource_path);
+
+    void parse(int argc, char *argv[]);
+
+protected:
+
+private:
+    bool _display_help;
+
+    std::string _action;
+
+    std::string _controller;
+
+    std::string _user;
+
+    std::string _password;
+
+    std::string _token;
+
+    std::string _json_file;
+
+    std::string _json_string;
+
+    std::string _ressource_path;
+
+};
+
+} // namespace camscli
+
+} // namespace libcams
+
+#endif // _f0a9ee97_83a8_4aa1_be9d_432ad9054c1c
