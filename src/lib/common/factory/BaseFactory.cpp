f3189c88606904cd28d20ec4edc617a2fd7ce35f
lahaxe <rom.lahaxe@gmail.com>
2020-03-28 14:04:29 +0100

    Ajout de la classe BaseFactory

---
+++ src/lib/common/factory/BaseFactory.cpp
@@ -0,0 +1,49 @@
+// Include Project files
+#include "common/factory/BaseFactory.h"
+
+namespace libcams
+{
+
+namespace common
+{
+
+template<class T>
+BaseFactory<T>
+::BaseFactory():
+    _creators(CreatorMap())
+{
+    // Nothing to do
+}
+
+template<class T>
+BaseFactory<T>
+::~BaseFactory()
+{
+    // Nothing to do
+}
+
+template<class T>
+bool
+BaseFactory<T>
+::can_create(std::string const & name) const
+{
+    typename CreatorMap::const_iterator const creators_it = this->_creators.find(name);
+    return (creators_it != this->_creators.end());
+}
+
+template<class T>
+std::shared_ptr<T>
+BaseFactory<T>
+::create(std::string const & name) const
+{
+    typename CreatorMap::const_iterator const creators_it = this->_creators.find(name);
+    if (creators_it != this->_creators.end())
+    {
+        return creators_it->second();
+    }
+    return nullptr;
+}
+
+} // namespace common
+
+} // namespace libcams
