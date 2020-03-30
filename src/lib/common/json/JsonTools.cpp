0476e7b5d14784b48d489a66e8740996cd93a2aa
lahaxe <rom.lahaxe@gmail.com>
2020-03-30 13:12:13 +0200

    Ajout de fonctions pour lire et écrire un fichier json

---
+++ src/lib/common/json/JsonTools.cpp
@@ -0,0 +1,55 @@
+#include <QFile>
+#include <QJsonDocument>
+
+// Include Project files
+#include "common/json/JsonTools.h"
+
+namespace common
+{
+
+namespace json
+{
+
+bool
+to_file(QJsonObject const & json, std::string const & filename)
+{
+    QFile file(QString(filename.c_str()));
+
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        return false;
+    }
+
+    QJsonDocument document(json);
+    if (file.write(document.toJson()) == -1)
+    {
+        file.close();
+        return false;
+    }
+
+    file.close();
+    return true;
+}
+
+bool
+from_file(QJsonObject & json, std::string const & filename)
+{
+    QFile file(QString(filename.c_str()));
+
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        return false;
+    }
+
+    auto data = file.readAll();
+    file.close();
+
+    auto document = QJsonDocument::fromJson(data);
+    json = document.object();
+
+    return true;
+}
+
+}
+
+}
